710e8e6679a3c8d1e2a8d2b386a6d6d32f1137fb
ElenaShapoval <Elena.Shapoval@syncretis.com>
2022-09-23 14:17:36 +0300

    changed names of the constants in the method checkFullscreenMode

--- src/test/java/SgpMainPageElementsTest.java
+++ src/test/java/SgpMainPageElementsTest.java
@@ -30,6 +30,6 @@ public class SgpMainPageElementsTest {
        assertThat(mainPage.header.getFullScreenBtn()).isVisible();
        mainPage.header.getFullScreenBtn().click();
        mainPage.getPage().waitForTimeout(2500);
-       Util.checkScreenshot("checkFullscreenModeOn", "FullscreenModeOn", "FullscreenModeOn");
+       Util.checkScreenshot("actualFullScreenModeOn", "FullscreenModeOn", "checkFullscreenModeOn");
     }
 }
