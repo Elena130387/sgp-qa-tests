4f53c8ca09189a2de06e15c551796c718cd0447d
elena.shapoval <alenchik-vseti@rambler.ru>
2022-11-25 18:30:10 +0300

    Small refactoring of resources structure and new test in GetShapeDataTest

--- src/test/java/backendTests/shape/GetShapeDataTest.java
+++ src/test/java/backendTests/shape/GetShapeDataTest.java
@@ -1,10 +1,18 @@
 package backendTests.shape;
 
+import api.dto.shape.Shape;
 import interfaces.GetByIdControllerTest;
+import io.restassured.mapper.ObjectMapperType;
+import org.junit.jupiter.api.Test;
 
+import static api.client.CalcManagement.getShapeDataById;
+import static api.helper.JsonHelper.getDataFromJsonFile;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static util.Constants.*;
 
 public class GetShapeDataTest implements GetByIdControllerTest {
+    private static final String TEST_SHAPE_FILE = "./src/test/resources/shapes/Orlando,Florida(15km).json";
+
     @Override
     public String getUrl() {
         return CALC_MAN_SHAPES_ID_EP;
@@ -18,5 +26,16 @@ public class GetShapeDataTest implements GetByIdControllerTest {
     @Override
     public int getDeletedId() {
         return DELETED_SHAPE_ID;
+    }
+
+    @Test
+    void checkResponseObjectStructure() {
+        Shape expShapeStructure = (Shape) getDataFromJsonFile(TEST_SHAPE_FILE, Shape.class);
+
+        Shape actShapeStructure = getShapeDataById(CORRECT_SHAPE_ID).extract()
+                .as(Shape.class, ObjectMapperType.GSON);
+
+        assertEquals(expShapeStructure,
+                actShapeStructure, "Актуальные данные области отличаются от ожидаемых");
     }
 }
