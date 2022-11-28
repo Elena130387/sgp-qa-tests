99b3b81a9f799473380531596d0ba3ce37c01312
elena.shapoval <alenchik-vseti@rambler.ru>
2022-11-28 13:43:31 +0300

    Small refactoring

--- src/test/java/backendTests/integration/SmallShapeWithOnePolygonTest.java
+++ src/test/java/backendTests/integration/SmallShapeWithOnePolygonTest.java
@@ -9,46 +9,39 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 import static api.client.CalcManagement.deleteShapeDataById;
 import static api.client.CalcManagement.getShapeDataById;
-import static api.client.Estimator.*;
 import static api.dto.StatusesList.DELETED;
-import static api.dto.StatusesList.STOPPED;
-import static api.helper.CalculationHelper.waitForCalculationStarting;
-import static api.helper.CalculationHelper.waitForCalculationStop;
 import static api.helper.JsonHelper.*;
 import static api.helper.PolygonHelper.verifyPolygonNumberAndCoordinates;
 import static api.helper.ShapeHelper.createShapeFromJson;
-import static java.lang.Thread.sleep;
+import static api.helper.ShapeHelper.waitForShapeStatusCompleted;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static util.Constants.CALCULATION_TIMEOUT_SEC;
 
-public class LargeShapeTest {
+
+public class SmallShapeWithOnePolygonTest {
     private String currentShapeName;
     private int shapeId;
     private ValidatableResponse responseCreateShape;
     private ShapeInput newShape;
-    private List<Integer> jobExecutionIds = new ArrayList<>();
     public static final int DURATION_SEC = 1;
-    private static final String LARGE_SHAPE_WITH_TWO_POLYGONS_FILE = "./src/test/resources/shapeInput/largeShapeWithTwoPolygons.json";
+    private static final String SMALL_SHAPE_WITH_ONE_POLYGON_FILE = "./src/test/resources/shapeInput/smallShapeWithOnePolygon.json";
 
     @BeforeEach
     public void createTestShape() throws TimeoutException {
-        newShape = (ShapeInput) getDataFromJsonFile(LARGE_SHAPE_WITH_TWO_POLYGONS_FILE, ShapeInput.class);
+        newShape = (ShapeInput) getDataFromJsonFile(SMALL_SHAPE_WITH_ONE_POLYGON_FILE, ShapeInput.class);
         newShape.AddDateToShapeName();
         responseCreateShape = createShapeFromJson(newShape);
         shapeId = getIntFromJson(responseCreateShape, "id");
-        waitForCalculationStarting(shapeId, CALCULATION_TIMEOUT_SEC, DURATION_SEC);
-        jobExecutionIds = getJobExecutionIds(shapeId, 2, 0);
+        waitForShapeStatusCompleted(shapeId, CALCULATION_TIMEOUT_SEC, DURATION_SEC);
     }
 
     @Test
-    void createLargeShape() {
+    void createSmallShape() {
         currentShapeName = getStringFromJson(responseCreateShape, "name");
         assertEquals(currentShapeName,
                 newShape.getName(), "У созданной области название не совпадает с заданным");
@@ -58,7 +51,7 @@ public class LargeShapeTest {
     }
 
     @Test
-    void renameCalculatingShape() {
+    void renameCompletedShape() {
         ShapeRename renameShape = new ShapeRename();
         ValidatableResponse responseRenameShape = CalcManagement.shapeRename(renameShape, shapeId);
         responseRenameShape.statusCode(200);
@@ -70,26 +63,13 @@ public class LargeShapeTest {
     }
 
     @Test
-    void deleteCalculatingShape() {
+    void deleteCompletedShape() {
         ValidatableResponse responseDeleteShape = deleteShapeDataById(shapeId);
         responseDeleteShape.statusCode(200);
 
         ValidatableResponse responseGetNewShapesData = getShapeDataById(shapeId);
         assertEquals(DELETED.getStatusName(),
                 getStringFromJson(responseGetNewShapesData, "status"), "Область не была удалена");
-    }
-
-    @Test
-    void stopCalculatingShape() throws TimeoutException {
-        for (Integer jobExecutionId : jobExecutionIds) {
-            stopJobExecutionById(jobExecutionId);
-        }
-
-        waitForCalculationStop(shapeId, 10, DURATION_SEC);
-
-        ValidatableResponse responseGetNewShapeData = getShapeDataById(shapeId);
-        assertEquals(STOPPED.getStatusName(),
-                getStringFromJson(responseGetNewShapeData, "status"), "Расчет области не был остановлен");
     }
 
 
@@ -101,13 +81,5 @@ public class LargeShapeTest {
         if (!testShapeStatus.equals(DELETED.getStatusName())) {
             ValidatableResponse responseDeleteShape = deleteShapeDataById(shapeId);
         }
-
-        try {
-            sleep(12000);
-        } catch (InterruptedException exception) {
-            System.out.println("Прервано ожидание обновления статуса области в базе данных");
-        }
-
-        ValidatableResponse responseDeleteJobExecutions = deleteJobExecutionsByIds(jobExecutionIds);
     }
 }
--- src/test/java/frontendTests/BuildingDamageCalcTest.java
+++ src/test/java/frontendTests/BuildingDamageCalcTest.java
@@ -7,11 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import pages.SgpMain;
 import pages.ShapeCalcPage;
-import util.Constants;
 import util.JunitExtension;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static util.Constants.SHAPE_DETAILS_URL_END;
+import static util.Constants.SHAPE_SHOW_PAGE_WITH_ASIDE_TRUE_URL;
 
 
 @ExtendWith(JunitExtension.class)
@@ -19,7 +20,7 @@ public class BuildingDamageCalcTest {
     SgpMain mainPage;
     ShapeCalcPage shapeCalcPage;
     private final int SHAPE_ID = 347;
-    private final String SHAPE_CALC_PAGE_URL = Constants.SHAPE_CHOOSE_URL_MID_TRUE + SHAPE_ID + Constants.SHAPE_DETAILS_URL_END;
+    private final String SHAPE_CALC_PAGE_URL = SHAPE_SHOW_PAGE_WITH_ASIDE_TRUE_URL + SHAPE_ID + SHAPE_DETAILS_URL_END;
     private final String TEST_SHAPE_NAME = "Oakland 6.58 кв. км";
     private final ShapeData shapeData = new ShapeData();
     private final DetailedShapePanel detailedShapePanel = new DetailedShapePanel();
