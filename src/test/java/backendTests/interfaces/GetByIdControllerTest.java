c820de7cbc10240a920961aa49fd9488438f41cd
elena.shapoval <alenchik-vseti@rambler.ru>
2022-11-28 13:37:47 +0300

    Added interface DeleteByIdControllerTest and tests for it. Made small refactoring of structure.

--- src/test/java/backendTests/interfaces/GetByIdControllerTest.java
+++ src/test/java/backendTests/interfaces/GetByIdControllerTest.java
@@ -1,4 +1,4 @@
-package interfaces;
+package backendTests.interfaces;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
---
+++ src/test/java/backendTests/shape/DeleteShapeDataByIdTest.java
@@ -0,0 +1,51 @@
+package backendTests.shape;
+
+import backendTests.interfaces.DeleteByIdControllerTest;
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.util.concurrent.TimeoutException;
+
+import static api.client.CalcManagement.deleteShapeDataById;
+import static api.client.CalcManagement.getShapeDataById;
+import static api.dto.StatusesList.DELETED;
+import static api.helper.JsonHelper.getStringFromJson;
+import static api.helper.ShapeHelper.createShapeFromFileAndGetID;
+import static api.helper.ShapeHelper.waitForShapeStatusCompleted;
+import static util.Constants.*;
+
+public class DeleteShapeDataByIdTest implements DeleteByIdControllerTest {
+    private static int shapeId;
+    private static final String SMALL_SHAPE_WITH_ONE_POLYGON_FILE = "./src/test/resources/shapeInput/smallShapeWithOnePolygon.json";
+    public static final int DURATION_SEC = 1;
+
+    @BeforeAll
+    public static void createTestShape() throws TimeoutException {
+        shapeId = createShapeFromFileAndGetID(SMALL_SHAPE_WITH_ONE_POLYGON_FILE);
+        waitForShapeStatusCompleted(shapeId, CALCULATION_TIMEOUT_SEC, DURATION_SEC);
+    }
+
+    @Override
+    public String getUrl() {
+        return CALC_MAN_SHAPES_ID_EP;
+    }
+
+    @Override
+    public int getCorrectId() {
+        return shapeId;
+    }
+
+    @Override
+    public int getDeletedId() {
+        return DELETED_SHAPE_ID;
+    }
+
+    @AfterAll
+    public static void deleteTestShape() {
+        String testShapeStatus = getStringFromJson(getShapeDataById(shapeId), "status");
+        if (!testShapeStatus.equals(DELETED.getStatusName())) {
+            ValidatableResponse responseDeleteShape = deleteShapeDataById(shapeId);
+        }
+    }
+}
